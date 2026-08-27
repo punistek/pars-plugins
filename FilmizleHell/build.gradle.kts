443a5faa1aaae7c2cd6a0d4661e48ec9bc2faba4
punistek <104331040+punistek@users.noreply.github.com>
2026-08-27 21:13:32 +0300

    Create build.gradle.kts

---
+++ FilmizleHell/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    description = "FilmizleHell film kaynağı"
+    authors = listOf("PARS")
+
+    status = 1
+    tvTypes = listOf(
+        "Movie"
+    )
+
+    iconUrl = "https://filmizlehell.com/favicon.ico"
+}
