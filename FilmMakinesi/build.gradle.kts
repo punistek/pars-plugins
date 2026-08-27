59570c36e1205fa77f15404c4fcc03f5cd5fcaeb
punistek <104331040+punistek@users.noreply.github.com>
2026-08-27 13:24:30 +0300

    Add initial build configuration for FilmMakinesi

---
+++ FilmMakinesi/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    description = "FilmMakinesi film kaynağı"
+    authors = listOf("PARS")
+
+    status = 1
+    tvTypes = listOf(
+        "Movie"
+    )
+
+    iconUrl = "https://filmmakinesi.to/favicon.ico"
+}
