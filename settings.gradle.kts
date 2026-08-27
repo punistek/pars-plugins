ac7cef6a1775e0e4ea0c255e1910f7b5fe8f32b1
punistek <104331040+punistek@users.noreply.github.com>
2026-08-27 13:22:28 +0300

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "PARS-Plugins"
+
+include(":FilmMakinesi")
