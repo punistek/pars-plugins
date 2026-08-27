5bce59ce3f3b0db9fad16366ece244bdbed1e2f7
punistek <104331040+punistek@users.noreply.github.com>
2026-08-27 18:56:25 +0300

    Update FilmMakinesiPlugin.kt

--- FilmMakinesi/src/main/kotlin/com/pars/filmmakinesi/FilmMakinesiPlugin.kt
+++ FilmMakinesi/src/main/kotlin/com/pars/filmmakinesi/FilmMakinesiPlugin.kt
@@ -8,5 +8,11 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class FilmMakinesiPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(FilmMakinesi())
+
+        // FilmMakinesi'nin kendi embed sunucuları.
+        // Bunlar kayıtlı olmazsa FilmMakinesi.loadLinks() içindeki
+        // loadExtractor(...) callback üretmez ve links=0 döner.
+        registerExtractorAPI(CloseLoadExtractor())
+        registerExtractorAPI(FilmMakinesiRapidExtractor())
     }
 }
