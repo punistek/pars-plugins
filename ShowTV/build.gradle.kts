a49da8c8ad6f89db8c34bd1b6c3b91c07853ff18
punistek <104331040+punistek@users.noreply.github.com>
2026-08-28 20:07:47 +0300

    Create build.gradle.kts

---
+++ ShowTV/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("PARS")
+    language    = "tr"
+    description = "Show TV dizilerini ve bölümlerini doğrudan ShowTV üzerinden otomatik listeler."
+
+    /**
+     * Status:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     */
+    status  = 1
+    tvTypes = listOf("TvSeries")
+
+    iconUrl = "https://mo.ciner.com.tr/assets/images/showtv-logo-400x400.png"
+}
