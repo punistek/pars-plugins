f0800662b1d259beed79bf36dcf09afca69f7499
punistek <104331040+punistek@users.noreply.github.com>
2026-08-28 11:40:35 +0300

    Create build.gradle.kts

---
+++ P720pizle/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+ language = "tr"
+ description = "720pizle kaynağı"
+ authors = listOf("PARS")
+ status = 1
+ tvTypes = listOf("Movie","TvSeries")
+ iconUrl = "https://720izle.com/favicon.ico"
+}
