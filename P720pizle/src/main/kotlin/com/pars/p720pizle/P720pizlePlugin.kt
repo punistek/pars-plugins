3a8414352d031f5561d51dc8c8ac75c240f5a57e
punistek <104331040+punistek@users.noreply.github.com>
2026-08-28 12:22:09 +0300

    Delete P720pizle directory

--- P720pizle/src/main/kotlin/com/pars/p720pizle/P720pizlePlugin.kt
+++
@@ -1,8 +0,0 @@
-package com.pars.p720pizle
-
-import android.content.Context
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-
-@CloudstreamPlugin
-class P720pizlePlugin:Plugin(){ override fun load(context:Context){ registerMainAPI(P720pizle()); registerExtractorAPI(P720pizleExtractor()) } }
