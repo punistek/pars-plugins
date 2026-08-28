015029fcf6f6b4004569f1fa4a9a1ccc333d0780
punistek <104331040+punistek@users.noreply.github.com>
2026-08-28 21:35:39 +0300

    Create build.gradle.kts

---
+++ Tizam/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "ru"
+    description = "Tizam provider"
+    authors = listOf("PARS")
+    status = 1
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://ane.tizam.org/templates/tizam/images/favicon.png"
+}
