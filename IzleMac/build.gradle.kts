600d90219eeb64c6fdf2893593889dce8355bac0
punistek <104331040+punistek@users.noreply.github.com>
2026-08-30 20:37:17 +0300

    Update description and icon URL in build.gradle.kts

--- IzleMac/build.gradle.kts
+++ IzleMac/build.gradle.kts
@@ -2,9 +2,9 @@ version = 1
 
 cloudstream {
     language = "tr"
-    description = "IzleMac TV kanal kataloğu"
+    description = "ArdaSpor canlı spor ve TV kanalları"
     authors = listOf("PARS")
     status = 1
     tvTypes = listOf("Live")
-    iconUrl = "https://izlemac549.sbs/favicon.ico"
+    iconUrl = "https://www.ardaspor30.top/assets/img/55557ardafav.png"
 }
