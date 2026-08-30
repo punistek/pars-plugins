816691e56e84afcffaead23a152e236b10b6f14d
punistek <104331040+punistek@users.noreply.github.com>
2026-08-30 11:14:32 +0300

    Create build.gradle.kts

---
+++ IzleMac/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    description = "IzleMac TV kanal kataloğu"
+    authors = listOf("PARS")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://izlemac549.sbs/favicon.ico"
+}
