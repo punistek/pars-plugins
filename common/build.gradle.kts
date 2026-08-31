637d8cd6c331412e6610325189849fd0429c63eb
punistek <104331040+punistek@users.noreply.github.com>
2026-08-31 22:16:57 +0300

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,7 +9,11 @@ plugins {
 android {
     namespace = "com.pars.common"
     compileSdk = 35
-    defaultConfig { minSdk = 21 }
+
+    defaultConfig {
+        minSdk = 21
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -21,7 +25,14 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-
+/*
+ * common modülünde Java ve Kotlin bytecode hedefleri aynı olmalı.
+ * GitHub Actions'taki hata:
+ *   compileDebugJavaWithJavac = 1.8
+ *   compileDebugKotlin       = 23
+ *
+ * Kotlin hedefini açıkça JVM 1.8'e sabitliyoruz.
+ */
 tasks.withType<KotlinJvmCompile>().configureEach {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_1_8)
