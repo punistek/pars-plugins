acd9f13470be7ccc13e634ca7533a4c8ca3d1963
punistek <104331040+punistek@users.noreply.github.com>
2026-08-31 09:50:32 +0300

    Add files via upload

---
+++ TEST/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    description = "TEST - Betmarino canlı kanal ve maç kaynağı"
+    authors = listOf("PARS")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.betmarinotv1101.site/favicon.ico"
+}
