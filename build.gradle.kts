1a4581697a8efc3dcec63187025bfe9c60f6990c
punistek <104331040+punistek@users.noreply.github.com>
2026-09-02 14:37:49 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        // DÜZELTİLDİ: Bozuk "-SNAPSHOT" tanımı "master-SNAPSHOT" olarak güncellendi.
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
