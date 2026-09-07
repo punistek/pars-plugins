e82fb6fe3d6539eb9fd1155ba1faed9b9e9a362d
punistek <104331040+punistek@users.noreply.github.com>
2026-09-07 22:22:58 +0300

    Delete filmmakinesi directory

--- filmmakinesi/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    language = "tr"
-    description = "Site Repo Builder tarafindan uretilen provider taslagi"
-    authors = listOf("SiteRepoBuilder")
-    status = 1
-    tvTypes = listOf("Movie", "TvSeries")
-}
