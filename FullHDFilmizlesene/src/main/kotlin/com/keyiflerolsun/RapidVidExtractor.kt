4dee27dd2fdd3b6fcf691ab2f7eadcc15b09086b
punistek <104331040+punistek@users.noreply.github.com>
2026-09-09 16:45:56 +0300

    Update RapidVidExtractor.kt

--- FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/RapidVidExtractor.kt
+++ FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/RapidVidExtractor.kt
@@ -13,13 +13,13 @@ open class RapidVid : ExtractorApi() {
 
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
         val extRef   = referer ?: ""
-        val videoReq = app.get(url, referer=extRef).text
+        val videoReq = app.get(url, referer = extRef).text
 
         val subUrls = mutableSetOf<String>()
         Regex("""captions","file":"([^"]+)","label":"([^"]+)"""").findAll(videoReq).forEach {
             val (subUrl, subLang) = it.destructured
 
-            if (subUrl in subUrls) { return@forEach }
+            if (subUrl in subUrls) return@forEach
             subUrls.add(subUrl)
 
             subtitleCallback.invoke(
@@ -30,20 +30,20 @@ open class RapidVid : ExtractorApi() {
             )
         }
 
-        var extractedValue   = Regex("""file": "(.*)",""").find(videoReq)?.groupValues?.get(1)
+        var extractedValue = Regex("""file": "(.*)",""").find(videoReq)?.groupValues?.get(1)
         val decoded: String?
 
         if (extractedValue != null) {
             val bytes = extractedValue.split("\\x").filter { it.isNotEmpty() }.map { it.toInt(16).toByte() }.toByteArray()
-            decoded   = String(bytes, Charsets.UTF_8)
+            decoded = String(bytes, Charsets.UTF_8)
         } else {
             val evalJWSsetup = Regex("""\};\s*(eval\(function[\s\S]*?)var played = \d+;""").find(videoReq)?.groupValues?.get(1) ?: throw ErrorLoadingException("File not found")
             @Suppress("LocalVariableName")
-            val JWSsetup      = getAndUnpack(getAndUnpack(evalJWSsetup)).replace("\\\\", "\\")
-            extractedValue  = Regex("""file":"(.*)","label""").find(JWSsetup)?.groupValues?.get(1)?.replace("\\\\x", "")
+            val JWSsetup = getAndUnpack(getAndUnpack(evalJWSsetup)).replace("\\\\", "\\")
+            extractedValue = Regex("""file":"(.*)","label"""").find(JWSsetup)?.groupValues?.get(1)?.replace("\\\\x", "")
 
             val bytes = extractedValue?.chunked(2)?.map { it.toInt(16).toByte() }?.toByteArray()
-            decoded   = bytes?.toString(Charsets.UTF_8) ?: throw ErrorLoadingException("File not found")
+            decoded = bytes?.toString(Charsets.UTF_8) ?: throw ErrorLoadingException("File not found")
         }
         Log.d("Kekik_${this.name}", "decoded » $decoded")
 
@@ -54,8 +54,8 @@ open class RapidVid : ExtractorApi() {
                 url    = decoded,
                 type   = ExtractorLinkType.M3U8
             ) {
-                referer = extRef
-                quality = Qualities.Unknown.value
+                this.referer = extRef
+                this.quality = Qualities.Unknown.value
             }
         )
     }
