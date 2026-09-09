057b11db59fd21e815ab831bb263db60583555cf
punistek <104331040+punistek@users.noreply.github.com>
2026-09-09 16:54:32 +0300

    Update TRsTXExtractor.kt

--- FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TRsTXExtractor.kt
+++ FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/TRsTXExtractor.kt
@@ -15,11 +15,11 @@ open class TRsTX : ExtractorApi() {
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
         val extRef = referer ?: ""
 
-        val videoReq = app.get(url, referer=extRef).text
+        val videoReq = app.get(url, referer = extRef).text
 
-        val file     = Regex("""file":"([^"]+)""").find(videoReq)?.groupValues?.get(1) ?: throw ErrorLoadingException("File not found")
+        val file     = Regex("""file":"([^"]+)"""").find(videoReq)?.groupValues?.get(1) ?: throw ErrorLoadingException("File not found")
         val postLink = "${mainUrl}/" + file.replace("\\", "")
-        val rawList  = app.post(postLink, referer=extRef).parsedSafe<List<Any>>() ?: throw ErrorLoadingException("Post link not found")
+        val rawList  = app.post(postLink, referer = extRef).parsedSafe<List<Any>>() ?: throw ErrorLoadingException("Post link not found")
 
         val postJson: List<TrstxVideoData> = rawList.drop(1).map { item ->
             val mapItem = item as Map<*, *>
@@ -36,21 +36,22 @@ open class TRsTX : ExtractorApi() {
             if (item.file == null || item.title == null) continue
 
             val fileUrl   = "${mainUrl}/playlist/" + item.file.substring(1) + ".txt"
-            val videoData = app.post(fileUrl, referer=extRef).text
+            val videoData = app.post(fileUrl, referer = extRef).text
 
-            if (videoData in vidLinks) { continue }
+            if (videoData in vidLinks) continue
             vidLinks.add(videoData)
 
-            vidMap.add(mapOf(
-                "title"     to item.title,
-                "videoData" to videoData
-            ))
+            vidMap.add(
+                mapOf(
+                    "title"     to item.title,
+                    "videoData" to videoData
+                )
+            )
         }
-
 
         for (mapEntry in vidMap) {
             Log.d("Kekik_${this.name}", "mapEntry » $mapEntry")
-            val title    = mapEntry["title"] ?: continue
+            val title   = mapEntry["title"] ?: continue
             val m3uLink = mapEntry["videoData"] ?: continue
 
             callback.invoke(
@@ -59,9 +60,9 @@ open class TRsTX : ExtractorApi() {
                     name   = "${this.name} - $title",
                     url    = m3uLink,
                     type   = INFER_TYPE
-                ) {
-                    referer = extRef
-                    quality = Qualities.Unknown.value
+                ) link@{
+                    this@link.referer = extRef
+                    this@link.quality = Qualities.Unknown.value
                 }
             )
         }
